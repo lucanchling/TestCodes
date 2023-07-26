2bf492ec2dd4cd1a7e2519d0fbe40e026b71e161
lucanchling <lanchlin@umich.edu>
2023-07-26 16:33:33 -0400

    Update CropCBCT

--- CropCBCT.py
+++ CropCBCT.py
@@ -1,12 +1,10 @@
 import SimpleITK as sitk
 from utils import search
 import numpy as np
-import json
-from tqdm import tqdm
-import os
+import os,json
 import multiprocessing as mp
 
-def Crop(ScanListe):
+def Crop(ScanListe, ROI_Path=None):
     for file in ScanListe:
         patient = os.path.basename(file).split("_Or")[0]
 
@@ -16,26 +14,28 @@ def Crop(ScanListe):
 
             img = sitk.ReadImage(file)
 
-            crop_target = sitk.ReadImage("/home/luciacev/Desktop/Luc_Anchling/TestCodes/Test/Cropped_Target.nii.gz")
-            ROI_Size = np.array(crop_target.GetSize()).astype(int)
-
-            ROI_Center = [3.8919315338134767, -25.656431198120118, 13.810585021972657]
-            # ROI_Size = [140, 115, 54]
+            if ROI_Path is not None: # When the Crop Volume ROI has been saved in a json file
+                ROI = json.load(open(ROI_Path))['markups'][0]
+                ROI_Center = np.array(ROI['center'])
+                ROI_Size = np.array(ROI['size'])
+            
+            else: # When you know the size and the center of the ROI
+                ROI_Center = [3.8919315338134767, -25.656431198120118, 13.810585021972657]
+                ROI_Size = [140, 115, 54]
 
             ROI_Center = np.array(img.TransformPhysicalPointToContinuousIndex(ROI_Center)).astype(int)
-            # ROI_Size = np.array(img.TransformContinuousIndexToPhysicalPoint(ROI_Size)).astype(int)
 
-            # Crop the image using the target
-
-            crop_image = img[ROI_Center[0]-ROI_Size[0]//2:ROI_Center[0]+ROI_Size[0]//2,
-                            ROI_Center[1]-ROI_Size[1]//2:ROI_Center[1]+ROI_Size[1]//2,
-                            ROI_Center[2]-ROI_Size[2]//2:ROI_Center[2]+ROI_Size[2]//2]
+            # Crop the image
+            crop_image = img[ROI_Center[0]-ROI_Size[0]*2:ROI_Center[0]+ROI_Size[0]*2,
+                            ROI_Center[1]-ROI_Size[1]*2:ROI_Center[1]+ROI_Size[1]*2,
+                            ROI_Center[2]-ROI_Size[2]*2:ROI_Center[2]+ROI_Size[2]*2]
+            
             try:
                 sitk.WriteImage(crop_image,ScanOutPath)
             except:
                 print("Error for patient: ",patient)
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     ScanListe = search("/home/luciacev/Desktop/Luc_Anchling/DATA/ASO_CBCT/NotOriented/SaraOr",".nii.gz")[".nii.gz"]
     ScanListe.sort()
 
